cf63c15aceaaf27fa523bd7e01090ae2ddb335b6
Ankit Agrawal <aggrawalankitsra@gmail.com>
2023-02-04 02:20:31 +0530

    add basic chat functionality using socket.io

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.use(cors())
 
 const PORT = process.env.PORT || 3000;
 
+// CHAT -- BEGIN
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
+// CHAT -- END
+
 app.get('/', (req, res)=>{
     res.send("Hi")
 })
@@ -19,9 +30,22 @@ app.use('/api/v1/sephora', productRouter);
 const userRouter = require('./routes/user');
 app.use('/api/v1/user', userRouter);
 
+// IO -- BEGIN
+var messagesDS = [];
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    io.to(socket.id).emit('messages history', messagesDS);
+    socket.on('chat message', (msg)=>{
+        console.log(msg);
+        messagesDS.push(msg);
+        socket.broadcast.emit('chat message', msg);
+    })
+});
+// IO -- END
+
 mongoose.connect(process.env.MONGO_DB_URL, ()=>{
     console.log("connected...");
-    app.listen(PORT, ()=>{
+    server.listen(PORT, ()=>{
         console.log("Listening...")
     })
 })
